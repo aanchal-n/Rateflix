94d3aaf5d1aa4e4e7ab32a25c0395f44ab3cab7c
aanchal-n <aanchalnarendran@gmail.com>
2020-12-03 22:58:27 +0530

    Modified Front end for movies

--- Myapp/src/components/layout/CardRow.js
+++ Myapp/src/components/layout/CardRow.js
@@ -1,21 +1,38 @@
 import React,{Component} from 'react';
-class CardRow extends Component{
-    render(){
-        return(
-            <div class="row">
+import BPimg from 'rateflix/src/Assets/posters/Movies/Black Panther.jpg'
+import BPaud from 'rateflix/src/Assets/Songs/Movies/Black Panther.jpg'
+var path=[{name:'Black Panther',
+			isrc: BPimg,
+			asrc: BPaud,}]
+
+class Card extends Component {
+	render() {
+		var i,imgsrc,audiosrc;
+        for(i=0;i<path.length;i++)
+        {
+            if(path[i].name === this.props.name)
+            {
+                imgsrc=path[i].isrc;
+                audiosrc=path[i].asrc;
+                break;
+            }
+        }
+        let audio= new Audio(audiosrc)
+        const start= () => {
+            audio.play();
+        }
+
+        const stop= () => {
+            audio.pause();
+        }
+		return (
 		<div class="col s16 m2">
 	      <div class="card red">
 		    <div class="card-image waves-effect waves-block waves-light">
-		  <img class="activator, waves-effect waves-light" alt="" id="BlackPantherimg" src="Assets/posters/Movies/Black Panther.jpg" width="300" height="300"></img>
-		  <audio src="Assets/Songs/Movies-Song/Black Panther.mp3" id="BlackAudio"> </audio>
-			<script type="text/javascript">
-				let blackimg=document.querySelector("#BlackPantherimg");
-				let blackaudio=document.querySelector("#BlackAudio");
-				audioplay(blackimg,blackaudio);
-			</script>
+		  <img class="activator, waves-effect waves-light" src={imgsrc} width="300" height="300" onMouseEnter={start} onMouseLeave={stop}></img>
 		</div>
 		<div class="card-content">
-		  <span class="card-title activator grey-text text-darken-4">Black Panther<i class="material-icons right">more_vert</i></span>
+		  <span class="card-title activator grey-text text-darken-4">{this.props.name}<i class="material-icons right">more_vert</i></span>
 		  <p class="tooltipped" data-position="bottom" data-tooltip="7.3"><span class="fa fa-star checked"></span>
 			<span class="fa fa-star checked"></span>
 			<span class="fa fa-star checked"></span>
@@ -24,113 +41,15 @@ class CardRow extends Component{
 		</div>
 	  </div>
 	  </div>
-	  <div class="col s16 m2">
-		<div class="card red">
-		  <div class="card-image waves-effect waves-block waves-light">
-		<img class="activator" alt="" id="JPImg" src="Assets/posters/Movies/Jurassic park.jpg" width="300" height="300"></img>
-		<audio src="Assets/Songs/Movies-Song/Jurassic park.mp3" id="JPAudio"> </audio>
-			<script type="text/javascript">
-				let jpimg=document.querySelector("#JPImg");
-				let jpaudio=document.querySelector("#JPAudio");
-				audioplay(jpimg,jpaudio);
-			</script>
-	  </div>
-	  <div class="card-content">
-		<span class="card-title activator grey-text text-darken-4">Jurassic park<i class="material-icons right">more_vert</i></span>
-		<p class="tooltipped" data-position="bottom" data-tooltip="8.1"><span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star"></span></p>
-			
-	</div>
-	</div>
-	</div>
-	<div class="col s16 m2">
-		<div class="card red">
-		  <div class="card-image waves-effect waves-block waves-light">
-		<img class="activator" alt="" id="TBCImg" src="Assets/posters/Movies/The breakfast club.jpg" width="300" height="300"></img>
-		<audio src="Assets/Songs/Movies-Song/The breakfast club.mp3" id="TBCAudio"> </audio>
-			<script type="text/javascript">
-				let tbcimg=document.querySelector("#TBCImg");
-				let tbcaudio=document.querySelector("#TBCAudio");
-				audioplay(tbcimg,tbcaudio);
-			</script>
-	  </div>
-	  <div class="card-content">
-		<span class="card-title activator grey-text text-darken-4">The Breakfast Club <i class="material-icons right">more_vert</i></span>
-		<p class="tooltipped" data-position="bottom" data-tooltip="7.8"><span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star"></span>
-			<span class="fa fa-star"></span></p>
-	</div>
-	</div>
-	</div>
-	<div class="col s16 m2">
-		<div class="card red">
-		  <div class="card-image waves-effect waves-block waves-light">
-		<img class="activator" alt="" id="ASIBImg" src="Assets/posters/Movies/A star is born.jpg" width="300" height="300"></img>
-		<audio src="Assets/Songs/Movies-Song/A star is born.mp3" id="ASIBAudio"> </audio>
-			<script type="text/javascript">
-				let asibimg=document.querySelector("#ASIBImg");
-				let asibaudio=document.querySelector("#ASIBAudio");
-				audioplay(asibimg,asibaudio);
-			</script>
-	  </div>
-	  <div class="card-content">
-		<span class="card-title activator grey-text text-darken-4">A Star is Born<i class="material-icons right">more_vert</i></span>
-		<p class="tooltipped" data-position="bottom" data-tooltip="7.6"><span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star"></span>
-			<span class="fa fa-star"></span></p>
-	</div>
-	</div>
-	</div>
-	<div class="col s16 m2">
-		<div class="card red">
-		  <div class="card-image waves-effect waves-block waves-light">
-		<img class="activator" alt="" id="TBImg" src="Assets/posters/Movies/The bodyguard.jpg" width="300" height="300"></img>
-		<audio src="Assets/Songs/Movies-Song/The bodyguard.mp3" id="TBAudio"> </audio>
-			<script type="text/javascript">
-				let tbimg=document.querySelector("#TBImg");
-				let tbaudio=document.querySelector("#TBAudio");
-				audioplay(tbimg,tbaudio);
-			</script>
-	  </div>
-	  <div class="card-content">
-		<span class="card-title activator grey-text text-darken-4">The bodyguard<i class="material-icons right">more_vert</i></span>
-		<p class="tooltipped" data-position="bottom" data-tooltip="6.3"><span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star"></span>
-			<span class="fa fa-star"></span></p>
-	</div>
-	</div>
-	</div>
-	<div class="col s16 m2">
-		<div class="card red">
-		  <div class="card-image waves-effect waves-block waves-light">
-		<img class="activator" alt="" id="SWImg" src="Assets/posters/Movies/Star wars.jpg" width="300" height="300"></img>
-		<audio src="Assets/Songs/Movies-Song/Star wars.mp3" id="SWAudio"> </audio>
-			<script type="text/javascript">
-				let swimg=document.querySelector("#SWImg");
-				let swaudio=document.querySelector("#SWAudio");
-				audioplay(swimg,swaudio);
-			</script>
-	  </div>
-	  <div class="card-content">
-		<span class="card-title activator grey-text text-darken-4">Star wars<i class="material-icons right">more_vert</i></span>
-		<p class="tooltipped" data-position="bottom" data-tooltip="8.6"><span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star checked"></span>
-			<span class="fa fa-star"></span></p>
-	 </div>
-	</div>
-	</div>
-	  </div>
+		)
+	}
+}
+class CardRow extends Component{
+    render(){
+        return(
+            <div class="row">
+				<Card name="Black Panther"/>
+	  		</div>
             )
         }
     }
