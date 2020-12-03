771d7d379da01e2c3c97a3f2afb3299c9d9b7adb
Akash Mehta <62140058+Kash6@users.noreply.github.com>
2020-12-03 21:56:09 +0530

    Create App.js

---
+++ Myapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import CardRow from './components/layout/CardRow';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <CardRow/>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
