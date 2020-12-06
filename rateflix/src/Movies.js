201a259ec53e218b1c0f5a8a1ed1653c7d2d5daa
Akash Mehta <62140058+Kash6@users.noreply.github.com>
2020-12-06 14:34:54 +0530

    Add files via upload

---
+++ rateflix/src/Movies.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React,{Component} from 'react';
+import Navbar from './Navbar';
+import CardRow from './CardRow';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <CardRow/>
+    </div>
+  );
+}
+
+export default App;
